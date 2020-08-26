5d128b243c3cf8758750e0dcde6618e20a41e9d7
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-08-26 09:44:09 +0100

    with code

--- src/main/java/tn/poste/data/project/services/AuthorService.java
+++ src/main/java/tn/poste/data/project/services/AuthorService.java
@@ -8,6 +8,7 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -46,9 +47,10 @@ public class AuthorService {
 				.collect(Collectors.toList());
 		
 			AuthorDto dto = new AuthorDto(author.getName(), author.getFirstName(), author.getTelphoneNumber(), books);
-		ModelMapper map= new ModelMapper();
-		
-			return map.map(author,AuthorDto.class);
+		ModelMapper modelMapper= new ModelMapper();
+		modelMapper.getConfiguration()
+		  .setMatchingStrategy(MatchingStrategies.STRICT);
+			return modelMapper.map(author,AuthorDto.class);
 	}
 	
 	public AuthorEntity createAuthor(AuthorEntity author) {
